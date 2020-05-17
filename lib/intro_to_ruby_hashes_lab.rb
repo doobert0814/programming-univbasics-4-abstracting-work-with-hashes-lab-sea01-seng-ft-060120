749a7bf54d0d0a38915ca7712c751141d4cebefe
Scot Sexton <rss3509@gmail.com>
2020-05-17 00:47:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -8,7 +8,8 @@ end
 
 def update_counting_hash(hash, key)
   hash[key]
-  hash["hello"]
+  if hash == NIL
+    return [1]
 
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
